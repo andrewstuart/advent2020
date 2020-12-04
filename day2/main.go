279169b986a5eb84058fc3514eeb29b43c357167
Andrew Stuart <andrew.stuart2@gmail.com>
2020-12-03 21:14:21 -0700

    Day2

---
+++ day2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.OpenFile("input", os.O_RDONLY, 0400)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	br := bufio.NewReader(f)
+
+	ct := 0
+	for {
+		st, err := br.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		var min, max int
+		var letter, str string
+		_, err = fmt.Sscanf(strings.TrimSpace(st), "%d-%d %s %s", &min, &max, &letter, &str)
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+
+		n := 0
+		ch := rune(letter[0])
+		for _, ch1 := range str {
+			if ch1 == ch {
+				n++
+			}
+		}
+		if min <= n && n <= max {
+			ct++
+		}
+	}
+	fmt.Println(ct)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module astuart.co/aoc2020
+
+go 1.15
